acfa6d24f6fc846c98529fb05b084896ada4d8ae
Somin Wadhwa <sominwadhwa@Somins-MacBook-Pro.local>
2017-08-07 15:13:20 +0530

    Data sources added via dropbox

---
+++ data/get_data.sh
@@ -0,0 +1,6 @@
+# Download the required versions of the datasets used in the project
+
+wget https://www.dropbox.com/s/yh0fwqb78wg8315/v1.zip?dl=1
+wget https://www.dropbox.com/s/u80uxh6px038i5b/v2.zip?dl=1
+
+unzip \*.zip
