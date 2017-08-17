3ebf873754889b95a2d8736a03fdc9bb7854ab66
Somin <sominwadhwa@gmail.com>
2017-08-18 02:11:14 +0530

    half way there

--- src/train_baseMLP.py
+++ src/train_baseMLP.py
@@ -1,14 +1,21 @@
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from random import shuffle
+import numpy as np
+import scipy.io
 from keras.optimizers import SGD
 from keras.utils import np_utils, generic_utils
 from sklearn.preprocessing import LabelEncoder
 from spacy.en import English
-from utils import group_together, freq_answers, ques_mat, ans_mat, img_mat
+from utils import freq_answers
 
 def main():
 
     training_questions = open("../preprocessed/ques_train.txt","rb").read().decode('utf8').splitlines()
     answers_train = open("../data/preprocessed/answer_train.txt","rb").read().decode('utf8').splitlines()
-    images_train = open()
+    images_train = open("../data/preprocessed/images_coco_id.txt","rb").read().decode('utf8').splitlines()
+    vgg_path = "/Users/sominwadhwa/Work/Minor/data/coco/vgg_feats.mat"
+    upper_lim = 1500 #Number of most frequently occurring answers in COCOVQA (80%+)
+    training_questions, answers_train, images_train = freq_answers(training_questions, answers_train, images_train, upper_lim)
+
+    print (shape(training_questions), shape(answers_train), shape(images_train))
